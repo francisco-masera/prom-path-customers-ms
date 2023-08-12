03ccbf3d562610fc4b9e4c4a427035853d24d297
Dargor94 <franciscomasera1994@gmail.com>
2023-08-12 15:52:16 -0300

    k8s

--- src/main/java/org/dargor/customer/CustomersMsApplication.java
+++ src/main/java/org/dargor/customer/CustomersMsApplication.java
@@ -2,11 +2,9 @@ package org.dargor.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableEurekaClient
 @EnableFeignClients
 @EnableJpaRepositories
 @SpringBootApplication
