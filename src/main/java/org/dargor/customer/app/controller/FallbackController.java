fb56309ea80dbdefa66316d4cde3b1e75a21b7af
Dargor94 <franciscomasera1994@gmail.com>
2023-09-15 14:11:51 -0300

    stop resilience

--- src/main/java/org/dargor/customer/app/controller/FallbackController.java
+++ src/main/java/org/dargor/customer/app/controller/FallbackController.java
@@ -4,13 +4,11 @@ import org.dargor.customer.app.exception.CustomException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import static org.dargor.customer.app.exception.ErrorDefinition.SERVICE_UNAVAILABLE;
 
-@RestController
-@RequestMapping("/fallback")
+//@RestController
+//@RequestMapping("/fallback")
 public class FallbackController {
 
     @GetMapping("/customer")
