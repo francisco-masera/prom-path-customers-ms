8e615385c805b2832e2d30b7071110ea8e613de2
Dargor94 <franciscomasera1994@gmail.com>
2023-10-19 00:17:28 -0300

    open telemetry removed

--- src/main/java/org/dargor/customer/app/config/TraceConfig.java
+++ src/main/java/org/dargor/customer/app/config/TraceConfig.java
@@ -2,14 +2,13 @@ package org.dargor.customer.app.config;
 
 import io.jaegertracing.internal.JaegerTracer;
 import io.jaegertracing.internal.samplers.ConstSampler;
-import io.opentelemetry.exporters.otlp.OtlpGrpcSpanExporter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class TraceConfig {
-    
+
     @Bean
     public JaegerTracer jaegerTracer(@Value("${spring.application.name}") String appName) {
 
@@ -18,11 +17,6 @@ public class TraceConfig {
                         .withParam(1))
                 .withReporter(new io.jaegertracing.Configuration.ReporterConfiguration().withLogSpans(true))
                 .getTracer();
-    }
-
-    @Bean
-    public OtlpGrpcSpanExporter otlpHttpSpanExporter(@Value("${tracing.url}") String url) {
-        return OtlpGrpcSpanExporter.newBuilder().setEndpoint(url).build();
     }
 
 
