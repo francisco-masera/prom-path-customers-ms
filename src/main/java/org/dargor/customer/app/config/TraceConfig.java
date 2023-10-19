b943b3e4679e53652efc131fa9b6e51468bd3e8d
Dargor94 <franciscomasera1994@gmail.com>
2023-10-18 23:38:02 -0300

    open telemetry

--- src/main/java/org/dargor/customer/app/config/TraceConfig.java
+++ src/main/java/org/dargor/customer/app/config/TraceConfig.java
@@ -2,24 +2,27 @@ package org.dargor.customer.app.config;
 
 import io.jaegertracing.internal.JaegerTracer;
 import io.jaegertracing.internal.samplers.ConstSampler;
+import io.opentelemetry.exporters.otlp.OtlpGrpcSpanExporter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class JaegerConfig {
-
-    @Value("${spring.application.name}")
-    private  String appName;
-
+public class TraceConfig {
+    
     @Bean
-    public JaegerTracer jaegerTracer() {
+    public JaegerTracer jaegerTracer(@Value("${spring.application.name}") String appName) {
 
         return new io.jaegertracing.Configuration(appName)
                 .withSampler(new io.jaegertracing.Configuration.SamplerConfiguration().withType(ConstSampler.TYPE)
                         .withParam(1))
                 .withReporter(new io.jaegertracing.Configuration.ReporterConfiguration().withLogSpans(true))
                 .getTracer();
+    }
+
+    @Bean
+    public OtlpGrpcSpanExporter otlpHttpSpanExporter(@Value("${tracing.url}") String url) {
+        return OtlpGrpcSpanExporter.newBuilder().setEndpoint(url).build();
     }
 
 
